e8fcb83d6709fafec96cead632f81f7f66f0c6cd
demure1u <aurelie.demure@telecomnancy.eu>
2023-10-17 10:32:11 +0200

    renommage

--- lexer/AutomateCaractere.java
+++ lexer/AutomateCaractere.java
@@ -2,7 +2,7 @@ package lexer; /*pour le mettre dans le package de l'analyseur lexicale*/
 
 import java.util.Scanner;
 
-public class reconnuCommeCaractere{
+public class AutomateCaractere{
     public static void main(String[] args){
         Scanner clavier = new Scanner(System.in);
         System.out.println("Entrez une séquence :");
