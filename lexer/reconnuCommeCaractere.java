4649a2f859f54c158099faebed1cf6a3c4b2963c
demure1u <aurelie.demure@telecomnancy.eu>
2023-10-17 09:18:43 +0200

    automate caractere codé

---
+++ lexer/reconnuCommeCaractere.java
@@ -0,0 +1,31 @@
+package lexer; /*pour le mettre dans le package de l'analyseur lexicale*/
+
+import java.util.Scanner;
+
+public class reconnuCommeCaractere{
+    public static void main(String[] args){
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Entrez une séquence :");
+        String input = clavier.nextLine();
+        clavier.close();
+        if (estReconnuCaractere(input)){ 
+            System.out.println("L'input est reconnu");
+        }
+        else{
+            System.out.println("L'input n'est pas reconnu");
+        }
+    }
+
+    public static boolean estReconnuCaractere(String input){
+        if (input.length() != 3) 
+            return false;
+        if(input.charAt(0) != '\'')
+            return false;
+        if (input.charAt(input.length()-1) != '\'') 
+            return false;
+        else {
+        return true;
+        }
+    }
+
+}
