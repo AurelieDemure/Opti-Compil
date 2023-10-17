8e65ac6d41984bc9248fd7343d5db0bc924afca8
demure1u <aurelie.demure@telecomnancy.eu>
2023-10-17 10:43:24 +0200

    modif caractere

--- lexer/AutomateCaractere.java
+++ lexer/AutomateCaractere.java
@@ -1,5 +1,5 @@
 package lexer; /*pour le mettre dans le package de l'analyseur lexicale*/
-
+ 
 import java.util.Scanner;
 
 public class AutomateCaractere{
@@ -8,23 +8,24 @@ public class AutomateCaractere{
         System.out.println("Entrez une séquence :");
         String input = clavier.nextLine();
         clavier.close();
-        if (estReconnuCaractere(input)){ 
+        if (estReconnuCaractere(input) != null){ 
             System.out.println("L'input est reconnu");
+            System.out.println(input);
         }
         else{
             System.out.println("L'input n'est pas reconnu");
         }
     }
 
-    public static boolean estReconnuCaractere(String input){
+    public static String estReconnuCaractere(String input){
         if (input.length() != 3) 
-            return false;
+            return null;
         if(input.charAt(0) != '\'')
-            return false;
+            return null;
         if (input.charAt(input.length()-1) != '\'') 
-            return false;
+            return null;
         else {
-        return true;
+        return input;
         }
     }
 
