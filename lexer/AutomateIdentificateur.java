3254a6b7fb75b5557802f683f3d3ea1a3609f8ba
Nicolas Fernandez <Nicolas.Fernandez@telecomnancy.eu>
2023-10-17 08:33:54 +0200

    Automate ident fini

--- lexer/AutomateIdentificateur.java
+++ lexer/AutomateIdentificateur.java
@@ -2,7 +2,7 @@ package lexer; /*pour le mettre dans le package de l'analyseur lexicale*/
 
 import java.util.Scanner;
 
-public class AutomateAlpha{
+public class AutomateIdentificateur{
     public static void main(String[] args){
         Scanner clavier = new Scanner(System.in);
         System.out.println("Entrez un caractere minuscule :");
@@ -17,17 +17,21 @@ public class AutomateAlpha{
     }
 
     public static boolean estReconnu(String input){
-        for (int i=0; i<input.length();i++){
-            char caractere = input.charAt(i);
-            if (!estCaractere(caractere)){
-                return false;
+        if(input.charAt(0)<'a'|| input.charAt(0)>'z')
+            return false;
+        else{
+            for (int i=1; i<input.length();i++){
+                char caractere = input.charAt(i);
+                if (!estCaractere(caractere)){
+                    return false;
+                }
             }
         }
         return true;
     }
 
     public static boolean estCaractere(char caractere){
-        if (caractere >='a' && caractere <='z')
+        if (caractere >='a' && caractere <='z' || caractere>='A' && caractere <='Z'|| caractere>='0'&& caractere<='9'|| caractere=='_')
             return true;
         else
             return false;
