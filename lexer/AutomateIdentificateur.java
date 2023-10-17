a4d478da27665d2d18c61c96eec8b5a0a0f927a3
Nicolas Fernandez <Nicolas.Fernandez@telecomnancy.eu>
2023-10-17 08:37:10 +0200

    Commentaires automate

--- lexer/AutomateIdentificateur.java
+++ lexer/AutomateIdentificateur.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class AutomateIdentificateur{
     public static void main(String[] args){
-        Scanner clavier = new Scanner(System.in);
+        Scanner clavier = new Scanner(System.in);                               /*Récupération caractère depuis le terminal */
         System.out.println("Entrez un caractere minuscule :");
         String input = clavier.nextLine();
         clavier.close();
@@ -17,10 +17,10 @@ public class AutomateIdentificateur{
     }
 
     public static boolean estReconnu(String input){
-        if(input.charAt(0)<'a'|| input.charAt(0)>'z')
+        if(input.charAt(0)<'a'|| input.charAt(0)>'z')           /*Vérification premier caractère lettre minuscule */
             return false;
         else{
-            for (int i=1; i<input.length();i++){
+            for (int i=1; i<input.length();i++){                            /*Vérification des autres caractères */
                 char caractere = input.charAt(i);
                 if (!estCaractere(caractere)){
                     return false;
