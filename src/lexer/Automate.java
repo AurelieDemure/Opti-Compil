4f9da02ace85505170e5277a6bca702fa9474ead
Nicolas Fernandez <Nicolas.Fernandez@telecomnancy.eu>
2023-10-25 17:02:28 +0200

    Automate Identificateur + classe Automate avec lecture caractère par caractère

--- src/lexer/Automate.java
+++ src/lexer/Automate.java
@@ -1,7 +1,29 @@
 package lexer;
 
 public class Automate {
-    String token;
-    String read;
-    char nextLexeur;
+    protected String token;
+    protected String read;
+    protected char nextLexeur;
+
+    public Automate(String token, String read, char nextLexeur){
+        this.token = token;
+        this.read = read;
+        this.nextLexeur = nextLexeur;
+    }
+
+    public void setToken(){
+        token = null;
+    }
+
+    public String getToken(){
+        return this.token;
+    }
+
+    public String getRead(){
+        return this.read;
+    }
+
+    public char getNextLexeur(){
+        return this.nextLexeur;
+    }
 }
--- src/lexer/AutomateIdentificateur.java
+++ src/lexer/AutomateIdentificateur.java
@@ -1,34 +1,30 @@
 package lexer; /*pour le mettre dans le package de l'analyseur lexicale*/
 
-public class AutomateIdentificateur{
-    public static void main(String[] args){
-        String input = args[0];
-        if (estReconnu(input)){ 
-            System.out.println("L'input est reconnu");
-        }
-        else{
-            System.out.println("L'input n'est pas reconnu");
+public class AutomateIdentificateur extends Automate{
+
+    public AutomateIdentificateur(String token, String read, char nextLexeur) {
+        super(token, read, nextLexeur);
+    }
+
+    public void estIdenticateur(char firstLexeur){
+        this.token += firstLexeur;
+        this.read += firstLexeur;
+        this.nextLexeur = (char)Lexer.read();
+        while(estReconnu(nextLexeur)){ 
+            this.token += nextLexeur;
+            this.read += nextLexeur;
+            this.nextLexeur = (char)Lexer.read();
         }
     }
 
-    public static boolean estReconnu(String input){
-        if(input.charAt(0)<'A'|| input.charAt(0)>'Z' && input.charAt(0)<'a' || input.charAt(0)>'z')           /*Vérification premier caractère lettre minuscule */
-            return false;
-        else{
-            for (int i=1; i<input.length();i++){                            /*Vérification des autres caractères */
-                char caractere = input.charAt(i);
-                if (!estCaractere(caractere)){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static boolean estCaractere(char caractere){
-        if (caractere >='a' && caractere <='z' || caractere>='A' && caractere <='Z'|| caractere>='0'&& caractere<='9'|| caractere=='_')
+    public boolean estReconnu(char nextLexeur){
+        if(nextLexeur >='a' && nextLexeur <='z' || nextLexeur>='A' && nextLexeur <='Z'|| nextLexeur>='0'&& nextLexeur<='9'|| nextLexeur=='_')         
             return true;
         else
             return false;
+    }
+
+    public void main(char firstLexeur){
+        estIdenticateur(firstLexeur);
     }
 }
