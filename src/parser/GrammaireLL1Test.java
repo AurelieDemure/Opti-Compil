45ba8bda2a5ade474726d281805899df8e8dd8ec
Manon Lécubin <Manon.Lecubin@telecomnancy.eu>
2024-01-09 18:14:44 -0500

    implementation fonctions semantiques 0 a 58

--- src/parser/GrammaireLL1Test.java
+++ src/parser/GrammaireLL1Test.java
@@ -17,13 +17,311 @@ public class GrammaireLL1Test extends Grammaire{
     //La classe Noeud est pas définie car nicolas a encore des trucs a faire, mais hesite pas à aller voir sur la branche parser_tree et notamment la classe Noeud pour les fonctions à utiliser
     public Component fonctionSemantique(int regle, Noeud noeud){
         switch(regle){
+            case 0:
+                Node node=new Node("Entete");
+                node.addChild(noeud.getFils(7));
+                node.addChild(noeud.getFils(9).interpret());
+                Node nodeFils1=new Node("block");
+                nodeFils1.addChild(noeud.getFils(11).interpret());
+                nodeFils1.addChild(noeud.getFils(12).interpret());
+                node.addChild(nodeFils1);
+                node.addChild(noeud.getFils(14).interpret());
+                return node;
+
+            case 1:
+                return null;
+                
+            case 2:
+                node=new Node("Declaration");
+                node.addChild(noeud.getFils(0).interpret());
+                node.addChild(noeud.getFils(1).interpret());
+                return node;
+                
+            case 3:
+                return null;
+
+            case 4:
+                node=new Node("Instanciation");
+                node.addChild(noeud.getFils(0).interpret());
+                node.addChild(noeud.getFils(1).interpret());
+                return node;
+
+            case 5:
+                return null;
+
+            case 6:
+                this.pile.add(noeud.getFils(0).interpret());
+                return null;
+
+            case 7:
+                this.pile.add(noeud.getFils(1));
+                return noeud.getFils(2).interpret();
+                
+            case 8:
+                node=new Node("Declaration");
+                node.addChild(noeud.getFils(0));
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(3).interpret());
+                node.addChild(noeud.getFils(4).interpret());
+                return node;
+                
+            case 9:
+                node=new Node("Procedure");
+                node.addChild(noeud.getFils(1));
+                node.addChild(noeud.getFils(2).interpret());
+                node.addChild(noeud.getFils(4).interpret());
+                nodeFils1=new Node("Block");
+                nodeFils1.addChild(noeud.getFils(6).interpret());
+                nodeFils1.addChild(noeud.getFils(7).interpret());
+                node.addChild(nodeFils1);
+                node.addChild(noeud.getFils(9).interpret());
+                return node;
+
+            case 10:
+                node=new Node("Fonction");
+                node.addChild(noeud.getFils(1));
+                node.addChild(noeud.getFils(2).interpret());
+                node.addChild(noeud.getFils(4).interpret());
+                node.addChild(noeud.getFils(6).interpret());
+                nodeFils1=new Node("Block");
+                nodeFils1.addChild(noeud.getFils(8).interpret());
+                nodeFils1.addChild(noeud.getFils(9).interpret());
+                node.addChild(nodeFils1);
+                node.addChild(noeud.getFils(11).interpret());
+                return node;
+
+            case 11:
+                return this.pile.remove();
+
+            case 12:
+                return noeud.getFils(1).interpret();
+                
+            case 13:
+                this.pile.add(noeud.getFils(1));
+                return this.pile.remove();
+                
+            case 14:
+                node=new Node("Record");
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(2).interpret());
+                return node;
+                
+            case 15:
+                return this.pile.remove();
+                
+            case 16:
+                node=new Node("Champ");
+                node.addChild(noeud.getFils(0).interpret());
+                node.addChild(noeud.getFils(1).interpret());
+                return node;
+
+            case 17:
+                node=new Node("Suite d'ident");
+                node.addChild(this.pile.remove());
+                node.addChild(noeud.getFils(2).interpret());
+                this.pile.add(noeud.getFils(1));
+                return node;
+
+            case 18:
+                return null;
+                
+            case 19:
+                return this.pile.remove();
+                
+            case 20:
+                node=new Node("Expression");
+                node.addChild(this.pile.remove());
+                node.addChild(noeud.getFils(1).interpret());
+                return node;
+                
+            case 21:
+                return noeud.getFils(0).interpret();
+
+            case 22:
+                return null;
+                
+            case 23:
+                node=new Node("Champ");
+                node.addChild(noeud.getFils(0));
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(3).interpret());
+                return node;
+                
+            case 24:
+                this.pile.add(noeud.getFils(0));
+                return this.pile.remove();
+                
+            case 25:
+                this.pile.add(noeud.getFils(1));
+                return this.pile.remove();
+
+            case 26:
+                node=new Node("Parametres");
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(2).interpret());
+                return node;
+
+            case 27:
+                return this.pile.remove();
+                
+            case 28:
+                //a voir
+
+            case 29:
+                node=new Node("parametres");
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(3).interpret());
+                node.addChild(noeud.getFils(4).interpret());
+                this.pile.add(noeud.getFils(0));
+                return node;
+                
+            case 30:
+                return null;
+
+            case 31:
+                return noeud.getFils(0).interpret();
+
+            case 32:
+                return noeud.getFils(1).interpret();
+                
+            case 33:
+                return null;
+
+            case 34:
+                return null;
+
+            case 35:
+                return noeud.getFils(0).interpret();
+
+            case 36:
+                //a voir
+                this.pile.add(noeud.getFils(0).interpret());
+                return noeud.getFils(1).interpret();
+
+            case 37:
+                node=new Node("Ou");
+                node.addChild(this.pile.remove());
+                node.addChild(noeud.getFils(1).interpret());
+                return node;
+
+            case 38:
+                return this.pile.remove();
+                
+            case 39:
+                //a voir
+                this.pile.add(noeud.getFils(0).interpret());
+                return noeud.getFils(1).interpret();
+
+            case 40:
+                //a voir
+                this.pile.add(noeud.getFils(1).interpret());
+                return noeud.getFils(2).interpret();
+
+            case 41:
+                //a voir
+                this.pile.add(noeud.getFils(0).interpret());
+                return noeud.getFils(1).interpret();
+
+            case 42:
+                node=new Node("And");
+                node.addChild(this.pile.remove());
+                node.addChild(noeud.getFils(1).interpret());
+                return node;
+
+            case 43:
+                return this.pile.remove();
+                
+            case 44:
+                //a voir
+                this.pile.add(noeud.getFils(0).interpret());
+                return noeud.getFils(1).interpret();
+    
+            case 45:
+                //a voir
+                this.pile.add(noeud.getFils(1).interpret());
+                return noeud.getFils(2).interpret();
+
+            case 46:
+                //a voir
+                this.pile.add(noeud.getFils(0).interpret());
+                return noeud.getFils(1).interpret();
+
+            case 47:
+                node=new Node("Negation");
+                node.addChild(this.pile.remove());
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(2).interpret());
+                return node;
+
+            case 48:
+                return this.pile.remove();
+                
+            case 49:
+                //a voir
+                this.pile.add(noeud.getFils(0).interpret());
+                return noeud.getFils(1).interpret();
+   
+            case 50:
+                node=new Node("Egalite");
+                node.addChild(this.pile.remove());
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(2).interpret());
+                return node;
+
+            case 51:
+                node=new Node("Inegalite");
+                node.addChild(this.pile.remove());
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(2).interpret());
+                return node;
+
+            case 52:
+                return this.pile.remove();
+
+            case 53:
+                //a voir
+                this.pile.add(noeud.getFils(0).interpret());
+                return noeud.getFils(1).interpret();
+
+            case 54:
+                node=new Node("Sup");
+                node.addChild(this.pile.remove());
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(2).interpret());
+                return node;
+
+            case 55:
+                node=new Node("SupEgal");
+                node.addChild(this.pile.remove());
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(2).interpret());
+                return node;
+
+            case 56:
+                node=new Node("Inf");
+                node.addChild(this.pile.remove());
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(2).interpret());
+                return node;
+
+            case 57:
+                node=new Node("InfEgal");
+                node.addChild(this.pile.remove());
+                node.addChild(noeud.getFils(1).interpret());
+                node.addChild(noeud.getFils(2).interpret());
+                return node;
+
+            case 58:
+                return this.pile.remove();
+
+
             case 59:
                 this.pile.add(noeud.getFils(0).interpret());//pour empiler
                 return noeud.getFils(1).interpret();//on interprete la suite
 
             case 60:
                 //Création du noeud
-                Node node = new Node("Somme");
+                node = new Node("Somme");
                 //ajout du premier fils (dépile)
                 node.addChild(pile.remove());
                 this.pile.add(noeud.getFils(1).interpret());//pour empiler
